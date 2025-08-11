f528f03104ed7e5270fdefc6c1de66a57987da6a
Farhan Fahim <1.fahimcoc@gmail.com>
2025-08-11 06:05:49 +0600

    Final update

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ const App = () => {
 
   return (
     <div>
+      <ToastContainer theme='dark' />
       <Routes>
-      <ToastContainer theme='dark'/>
         <Route path='/' element={<Home/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/player/:id' element={<Player/>}/> 
